65537871db8155bf7ae82e7d46320ab719e47db6
Jacob <j.romanowski133@gmail.com>
2024-12-08 23:30:34 +0100

    Lab 5 9/9
    GG WP

--- Lab5/frontend-angular/src/app/app.routes.ts
+++ Lab5/frontend-angular/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { BrandsComponent } from './brands/brands.component';
 import { NgModule } from '@angular/core';
-import { CarsComponent } from './cars/cars.component';
 import { BrandDetailComponent } from './brand-detail/brand-detail.component';
 import { EditBrandComponent } from './edit-brand/edit-brand.component';
 import { EditCarComponent } from './edit-car/edit-car.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 
 export const routes: Routes = [
-    { path: 'cars', component: CarsComponent },
     { path: 'brands', component: BrandsComponent},
     { path: 'brands/:uuid', component: BrandDetailComponent},
     { path: 'brands/:uuid/edit', component: EditBrandComponent},
-    { path: 'cars/:uuid/edit', component: EditCarComponent}
+    { path: 'cars/:uuid/edit', component: EditCarComponent},
+    { path: 'cars/:uuid/details', component: CarDetailComponent}
 ];
--- Lab5/frontend-angular/src/app/car-detail/car-detail.component.ts
+++ Lab5/frontend-angular/src/app/car-detail/car-detail.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-car-detail',
@@ -10,5 +11,49 @@ import { RouterModule } from '@angular/router';
   styleUrl: './car-detail.component.css'
 })
 export class CarDetailComponent {
+  car: any;
+  carId: string | null = null;
+  brand: any;
+  errorMessage = "";
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute,private router: Router) {
+    this.route.paramMap.subscribe(params => {
+    this.carId = params.get('uuid');  // Pobieramy 'uuid' z URL
+    if (this.carId) {
+      this.getCar();  // Wywołanie metody z id
+      this.getBrand();
+    }
+    });
+  }
+
+  getCar(): void{
+    this.apiService.getCars().subscribe({
+      next: (allCars) => {
+        console.log('Odpowiedź z API (wszystkie samochody):', allCars);
+        // Filtrowanie samochodów pasujących do UUID z `this.carIds`
+        this.car = allCars.find((car: any) => this.carId === car.id);
+        console.log('Samochód dopasowany do UUID:' + this.car.model);
+      },
+      error: (err) => {
+        this.errorMessage = 'Wystąpił błąd podczas ładowania wszystkich samochodów!';
+        console.error(err);
+      }
+    });
+  }
+
+  getBrand(): void{
+    this.apiService.getBrands().subscribe({
+      next: (allBrands) => {
+        console.log('Odpowiedź z API (wszystkie marki):', allBrands);
+        // Filtrowanie samochodów pasujących do UUID z `this.carIds`
+        this.brand = allBrands.find((brand: any) => this.car.brandId === brand.id);
+        console.log('Marka dopasowana do UUID:' + this.brand.name);
+      },
+      error: (err) => {
+        this.errorMessage = 'Wystąpił błąd podczas ładowania wszystkich marek!';
+        console.error(err);
+      }
+    });
+  }
 
 }
